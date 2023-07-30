7882efa868cf915ea0af930c2bb8ff565c8be2b4
Yan Kida <theyaaans@outlook.com>
2023-07-30 22:51:15 +0100

    Adding better column design

--- src/main/java/com/shooting/ShootingApplication.java
+++ src/main/java/com/shooting/ShootingApplication.java
@@ -3,6 +3,7 @@ package com.shooting;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 //Test Pushing from Laptop
 @SpringBootApplication
 public class ShootingApplication {
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Fetching data
-    fetch('/Incident') // API Endpoint
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => displayData(data))
-        .catch(error => {
-            console.error('Error fetching data:', error);
-            displayErrorMessage();
-        });
+    function fetchData() {
+        fetch('/Incident') // API Endpoint
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => displayData(data))
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                displayErrorMessage();
+            });
+    }
 
     // Function to display data in the table
     function displayData(data) {
@@ -21,10 +23,13 @@ document.addEventListener('DOMContentLoaded', () => {
         data.forEach(incident => {
             const row = document.createElement('tr');
 
-            const incidentKeyCell = document.createElement('td');
-            incidentKeyCell.textContent = incident.INCIDENT_KEY !== undefined ? incident.INCIDENT_KEY : JSON.stringify(incident);
-
-            row.appendChild(incidentKeyCell);
+            // Display each property in the table cells
+            const keys = ['incident_KEY', 'occur_DAT', 'vic_RACE', 'vic_SEX', 'boro', 'precint'];
+            keys.forEach(key => {
+                const cell = document.createElement('td');
+                cell.textContent = incident[key] !== null ? incident[key] : 'N/A';
+                row.appendChild(cell);
+            });
 
             tableBody.appendChild(row);
         });
@@ -33,6 +38,53 @@ document.addEventListener('DOMContentLoaded', () => {
     // Function to display an error message in the table if data fetching fails
     function displayErrorMessage() {
         const tableBody = document.querySelector('#incidentTable tbody');
-        tableBody.innerHTML = '<tr><td colspan="3">Error fetching data. Please try again later.</td></tr>';
+        tableBody.innerHTML = '<tr><td colspan="6">Error fetching data. Please try again later.</td></tr>';
     }
+
+    // Show incident form when "Add" button is clicked
+    const addButton = document.getElementById('addButton');
+    const incidentForm = document.getElementById('incidentForm');
+
+    addButton.addEventListener('click', () => {
+        incidentForm.style.display = 'block';
+    });
+
+    // Handle form submission
+    const submitButton = document.getElementById('submitButton');
+    submitButton.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Get values from the form
+        const incidentKeyInput = document.getElementById('incidentKey');
+        const incidentDateInput = document.getElementById('incidentDate');
+        const victimRaceInput = document.getElementById('victimRace');
+        const victimSexInput = document.getElementById('victimSex');
+        const boroughInput = document.getElementById('borough');
+        const precinctInput = document.getElementById('precinct');
+
+        // Create an object with the incident data
+        const newIncident = {
+            "incident_KEY": incidentKeyInput.value.trim(),
+            "occur_DAT": incidentDateInput.value.trim(),
+            "vic_RACE": victimRaceInput.value.trim(),
+            "vic_SEX": victimSexInput.value.trim(),
+            "boro": boroughInput.value.trim(),
+            "precint": parseInt(precinctInput.value.trim()) || 0
+        };
+
+        // Here, you can send the newIncident object to your server using fetch() or other methods to save it to the database.
+
+        // After successfully adding the incident, you may want to clear the form and update the incident table.
+        incidentKeyInput.value = '';
+        incidentDateInput.value = '';
+        victimRaceInput.value = '';
+        victimSexInput.value = '';
+        boroughInput.value = '';
+        precinctInput.value = '';
+        incidentForm.style.display = 'none';
+        fetchData(); // Fetch and display data again to update the table
+    });
+
+    // Fetch data on page load
+    fetchData();
 });
